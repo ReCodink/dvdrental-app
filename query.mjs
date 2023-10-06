24ebdf39f0508e2f6e45b387e300e8200067bd2f
ReCodink <muhammadraihan1607@gmail.com>
2023-10-06 14:20:09 +0700

    Fungsi untuk menampilkan database

--- query.mjs
+++ query.mjs
@@ -4,7 +4,7 @@ export const getAllFilmsQuery = 'SELECT * FROM film';
 export const getFilmByIdQuery = 'SELECT * FROM film WHERE film_id = $1';
 export const getAllCategoriesQuery = 'SELECT * FROM category';
 export const getFilmsByCategoryQuery = `
-    SELECT c.name AS category_name, f.title AS film_title, fc.last_update
+    SELECT f.title AS film_title, c.name AS category_name, fc.last_update
     FROM film_category AS fc
     JOIN film AS f ON fc.film_id = f.film_id
     JOIN category AS c ON fc.category_id = c.category_id;
